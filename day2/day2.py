1f603dd7e23cfc28ff66b02c43d85be3cb7f4c19
Patrixxx <heerorama@gmail.com>
2021-12-16 21:45:53 +0100

    refactor day2

--- day2/day2.py
+++ day2/day2.py
@@ -2,32 +2,29 @@
 
 data = open("day2.txt")
 #part1
-pos = 0
-depth = 0
+pos = depth = 0
 for line in data:
-    command, value = line.split( )
+    command, value = line.split()
     value = int(value)
     if command == 'forward':
-        pos = pos + value
+        pos += value
     elif command == 'down':
-        depth = depth + value
+        depth += value
     elif command == 'up':
-        depth = depth - value
+        depth -= value
 print('Awnser Part1 ', pos * depth)
 
 #part2
 data = open("day2.txt")
-pos = 0
-depth = 0
-aim = 0
+pos = depth = aim = 0
 for line in data:
-    command, value = line.split( )
+    command, value = line.split()
     value = int(value)
     if command == 'forward':
-        pos = pos + value
-        depth = depth + (aim * value)
+        pos += value
+        depth += (aim * value)
     elif command == 'down':
-        aim = aim + value
+        aim += value
     elif command == 'up':
-        aim = aim - value
+        aim -= value
 print('Awnser Part2 ', pos * depth)
